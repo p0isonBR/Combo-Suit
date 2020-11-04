3fdc48d54b098f802b9172dd38db4a266fe64912
p0isonBR <65384918+p0isonBR@users.noreply.github.com>
2020-11-04 08:03:15 -0300

    Update comboparser.py

--- comboparser.py
+++ comboparser.py
@@ -1,5 +1,4 @@
 import re, os
-from datetime import datetime
 
 workdir=os.path.join('/', 'sdcard', 'ComboSuitByPoisonBR')
 if not os.path.exists(workdir):
@@ -29,33 +28,30 @@ def change(combo):
 
 print('''Tools:
 1 > separar combos por dominio (gmail, hotmail, yahoo...)
-2 > Trocar separador : para |.
-3 > Trocar separador | para :.
+2 > Trocar separador | para : e vice-versa.
 ''')
 
-tool=input('Selecione a tool (1,2 ou 3): ')
+tool=input('Selecione a tool (1 ou 2): ')
 
 db=open(input('Caminho da DB: '), 'r').read().splitlines()
 
 if tool=='1':
-    dir=os.path.join('/', 'sdcard', 'ComboSuitByPoisonBR', 'PorDominio', str(datetime.today())[0:10])
+    dir=input('Digite um nome para a pasta de saida: ')
+    dir=os.path.join('/', 'sdcard', 'ComboSuitByPoisonBR', 'PorDominio',dir)
     if not os.path.exists(dir):
         os.mkdir(dir)
     for combo in db:
         host=re.search('@(.*):', combo).group(1)
         sepdom(host, combo)
+
 elif tool=='2':
-    txt=sepdir+'/Out-'+str(datetime.now())[11:19]+'.txt'
+    txt=input('Defina o nome da nova DB: ')
+    txt=sepdir+'/'+txt+'.txt'
     for combo in db:
-        combo=combo.replace('|', ':')
+        if re.search(':', combo)==':':
+        	combo=combo.replace(':','|')
+        else:
+        	combo=combo.replace('|', ':')
         change(combo)
-elif tool=='3':
-    txt=sepdir+'/Out-'+str(datetime.now())[11:19]+'.txt'
-    for combo in db:
-        combo=combo.replace(':', '|')
-        change(combo)
-else:
-    print('a tool selecionada e invalida')
-    exit('tool invalida')
 
 print('Tarefa realizada!')
