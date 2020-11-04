bbd4baf00383c10688c4f0209dd2cbc079a70f1b
p0isonBR <65384918+p0isonBR@users.noreply.github.com>
2020-11-04 13:33:16 -0300

    Update comboparser.py

--- comboparser.py
+++ comboparser.py
@@ -31,49 +31,53 @@ print('''Tools:
 2 > Trocar separador | para : e vice-versa.
 ''')
 
-tool=input('Selecione a tool (1 ou 2): ')
+try:
+    tool=input('Selecione a tool (1 ou 2): ')
 
-db=open(input('Caminho da DB: '), 'rb').read().decode('utf-8',errors='ignore').splitlines()
+    db=open(input('Caminho da DB: '), 'rb').read().decode('utf-8',errors='ignore').splitlines()
 
-if tool=='1':
-    dir=input('Digite um nome para a pasta de saida: ')
-    dir=os.path.join('/', 'sdcard', 'ComboSuitByPoisonBR', 'PorDominio',dir)
-    if not os.path.exists(dir):
-        os.mkdir(dir)
-    try:
-        for combo in db:
-        	host=re.search('@(.*):', combo).group(1)
-        	sepdom(host, combo)
-    except(AttributeError):
-        	print('Voce deve trocar o separador antes (de | para :).'); time.sleep(3)
-        	ex=input('Deseja trocar agora? (y/n): ')
-        	if ex=='y' or ex=='Y' or ex=='yes' or ex=='Yes':
-        		new=input('Defina o nome do novo arquivo: ')
-        		txt=sepdir+'/'+new+'.txt'
-        		for combo in db:
-        			combo=combo.replace('|', ':')
-        			change(combo)
-        		db=open(txt, 'rb').read().decode('utf-8',errors='ignore').splitlines()
-        		try:
-        			for combo in db:
-        				host=re.search('@(.*):', combo).group(1)
-        				sepdom(host, combo)
-        		except:
-        			print('Tarefa realizada!')
-        			exit()
-        	else:
-        		print('Saindo...')
-        		exit()
-        	
-elif tool=='2':
-    txt=input('Defina o nome do novo arquivo: ')
-    txt=sepdir+'/'+txt+'.txt'
-    for combo in db:
+    if tool=='1':
+        dir=input('Digite um nome para a pasta de saida: ')
+        dir=os.path.join('/', 'sdcard', 'ComboSuitByPoisonBR', 'PorDominio',dir)
+        if not os.path.exists(dir):
+            os.mkdir(dir)
         try:
-        	if re.search(':', combo).group()==':':
-        		combo=combo.replace(':','|')
+            for combo in db:
+                    host=re.search('@(.*):', combo).group(1)
+                    sepdom(host, combo)
         except(AttributeError):
-        	combo=combo.replace('|',':')
-        change(combo)
+                    print('Voce deve trocar o separador antes (de | para :).'); time.sleep(3)
+                    ex=input('Deseja trocar agora? (y/n): ')
+                    if ex=='y' or ex=='Y' or ex=='yes' or ex=='Yes':
+                            new=input('Defina o nome do novo arquivo: ')
+                            txt=sepdir+'/'+new+'.txt'
+                            for combo in db:
+                                    combo=combo.replace('|', ':')
+                                    change(combo)
+                            db=open(txt, 'rb').read().decode('utf-8',errors='ignore').splitlines()
+                            try:
+                                    for combo in db:
+                                            host=re.search('@(.*):', combo).group(1)
+                                            sepdom(host, combo)
+                            except:
+                                    print('Tarefa realizada!')
+                                    exit()
+                    else:
+                            print('Saindo...')
+                            exit()
+                    
+    elif tool=='2':
+        txt=input('Defina o nome do novo arquivo: ')
+        txt=sepdir+'/'+txt+'.txt'
+        for combo in db:
+            try:
+                    if re.search(':', combo).group()==':':
+                            combo=combo.replace(':','|')
+            except(AttributeError):
+                    combo=combo.replace('|',':')
+            change(combo)
 
-print('Tarefa realizada!')
+    print('Tarefa realizada!')
+except(KeyboardInterrupt):
+    print('Cancelado pelo usuário.')
+    exit('Ctrl-C pressionado')
