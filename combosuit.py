b990e7c8514e6a4c6170e131dee595ba03f4d20b
p0isonBR <65384918+p0isonBR@users.noreply.github.com>
2020-11-08 03:59:59 -0300

    Update combosuit.py

--- combosuit.py
+++ combosuit.py
@@ -2,7 +2,7 @@ import re, os, time
 
 R='\033[1;31m'; B='\033[1;34m'; C='\033[1;37m'; Y='\033[1;33m'; G='\033[1;32m'; RT='\033[;0m'
 
-os.system('clear')
+os.system('git pull && clear')
 
 print(f'''{C}
                             /+osyhhhhhhyys++/
